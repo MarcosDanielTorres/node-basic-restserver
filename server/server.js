d12f7529a003bf6f7b6564c45a8bf6e1c0b97a52
Marcos Daniel Torres <mdaniel.torres7@gmail.com>
2019-12-29 16:15:36 -0300

    Corregido nombre de variable

--- server/server.js
+++ server/server.js
@@ -18,10 +18,10 @@ app.get('/usuario', function(req, res) {
 
 
 app.post('/usuario', function(req, res) {
-    let body = req.body
+    let usuario = req.body
 
     res.json({
-        body
+        usuario
     })
 })
 
